b9c635a84de9b260917b23b31f4ee752d9868510
gumybit <gumybit@gmail.com>
2023-03-17 12:02:00 +0000

    Fix formula

--- Formula/open-ai-cli.rb
+++ Formula/open-ai-cli.rb
@@ -1,7 +1,7 @@
 class OpenAiCli < Formula
   desc "Open AI API CLI able to chat/completion written in Rust"
   homepage "https://github.com/gumybit/open-ai-cli"
-  url "https://github.com/gumybit/homebrew-open-ai-cli/releases/download/v0.1.0/open-ai-cli-0.1.0-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/gumybit/open-ai-cli/releases/download/v0.1.0/open-ai-cli-0.1.0-x86_64-apple-darwin.tar.gz"
   sha256 "4d883f6cf5ba5764c2942955145ff060a9fa4e12d4a314d8418b37d91daf0450"
   version "0.1.0"
 
