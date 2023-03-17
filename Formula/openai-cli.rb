70c1c2e9fb4836faa8fd24b9bb589a9d471b7aca
gumybit <gumybit@gmail.com>
2023-03-17 12:19:29 +0000

    Fix formula

--- Formula/openai-cli.rb
+++ Formula/openai-cli.rb
@@ -1,7 +1,7 @@
 class OpenaiCli < Formula
   desc "OpenAI API CLI able to chat/completion written in Rust"
   homepage "https://github.com/gumybit/openai-cli"
-  url "https://github.com/gumybit/openai-cli/releases/download/v0.1.0/openai-cl.mac.tar.gz"
+  url "https://github.com/gumybit/openai-cli/releases/download/v0.1.0/openai-cli.mac.tar.gz"
   sha256 "ddc1a76dc244fea3688e75e776009c160fabc7e1b68b22f585ec44032ff29beb"
   version "0.1.0"
 
